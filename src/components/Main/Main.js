18905b9790f4700208256382ff31755f9437682a
ti-bui <buimydung2001@gmail.com>
2023-08-09 16:09:37 -0400

    responsive: nav, about, photoList, main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,27 +7,27 @@ import ScrollDown from "../ScrollDown/ScrollDown";
 gsap.registerPlugin(SplitText);
 
 const Main = () => {
-  let mySplitText = new SplitText(".main__mainHeader", { type: "chars" });
+  // let mySplitText = new SplitText(".main__mainHeader", { type: "chars" });
 
-  let chars = mySplitText.chars;
+  // let chars = mySplitText.chars;
 
-  useEffect(() => {
-    gsap.to(chars, {
-      duration: 1,
-      opacity: 1,
-      stagger: 0.1,
-      y: window.innerWidth > 768 ? "-40px" : "-20px",
-      ease: Power3.easeOut,
-    });
-  });
+  // useEffect(() => {
+  //   gsap.to(chars, {
+  //     duration: 1,
+  //     opacity: 1,
+  //     stagger: 0.1,
+  //     y: window.innerWidth > 768 ? "-40px" : "-20px",
+  //     ease: Power3.easeOut,
+  //   });
+  // });
 
   return (
-    <section className="main">
-      <div className="main__wrapper">
+    <>
+      <div className="main">
         <h1 className="main__mainHeader">Ti Bui</h1>
       </div>
-      <ScrollDown />
-    </section>
+      {/* <ScrollDown /> */}
+    </>
   );
 };
 
