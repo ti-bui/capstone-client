d6b23df90112d5b71d224abb4038f9350c067928
ti-bui <buimydung2001@gmail.com>
2023-08-14 23:42:19 -0400

    fix main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,11 @@
 import "./main.scss";
-import { useEffect } from "react";
-import { gsap, Power3 } from "gsap";
-import { SplitText } from "gsap/all";
-import ScrollDown from "../ScrollDown/ScrollDown";
-
-gsap.registerPlugin(SplitText);
 
 const Main = () => {
-  let mySplitText = new SplitText(".main__mainHeader", { type: "chars" });
-
-  let chars = mySplitText.chars;
-
-  // useEffect(() => {
-  //   gsap.to(chars, {
-  //     duration: 1,
-  //     opacity: 1,
-  //     stagger: 0.1,
-  //     y: window.innerWidth > 768 ? "-3%" : "-20px",
-  //     // y: "-3%",
-  //     ease: Power3.easeOut,
-  //   });
-  // });
-
   return (
     <>
       <div className="main">
         <h1 className="main__mainHeader">Ti Bui</h1>
       </div>
-      <ScrollDown />
     </>
   );
 };
