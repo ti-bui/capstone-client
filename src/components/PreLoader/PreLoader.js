d131bcbba275fc711996e0328371c7e50d0196c4
ti-bui <buimydung2001@gmail.com>
2023-08-15 01:12:11 -0400

    add preloader

--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -1,7 +1,11 @@
 import "./preloader.scss";
 
 const PreLoader = () => {
-  return <div className="preloader"></div>;
+  return (
+    <div className="preloader">
+      <p className="preloader__text">Beauty is loading...</p>
+    </div>
+  );
 };
 
 export default PreLoader;
